78486b73dff9abd6eec657361da2c923f50c788c
jbouwh <jan@jbsoft.nl>
2022-04-22 07:42:30 +0000

    update setup

--- setup.py
+++ setup.py
@@ -15,12 +15,10 @@ def get_version(package):
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-install_requires = [
-    "pyyaml>=6.0",
-]
+install_requires = []
 
 setup(
     name="lib-elro-connects",
